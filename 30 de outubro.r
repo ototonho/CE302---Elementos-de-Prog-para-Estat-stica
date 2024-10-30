459294c252d6a5281a23b7f20e4bec21ba4b7815
ototonho <antonio.steffen@ufpr.br>
2024-10-30 20:31:32 -0300

    aula ao final

--- 30 de outubro.r
+++ 30 de outubro.r
@@ -64,7 +64,151 @@ TB1
 TB1 %>% 
   count(chave) %>% View()
 
+#filtro para variáveis com new
 TB1 %<>% filter(chave %like% "^new") #para ver funções com essa variável
 View(TB1)
 
+#separando o new sp e afins em diferentes variáveis
+TB2 <- TB1 %>% 
+  mutate(chave = stringr::str_replace(chave, "newrel", "new_rel")) #stringr função nova
+TB2
 
+TB3 <- TB2 %>% 
+  separate(chave, c("new", "type", "sexage"), 
+           sep = "_") #separando variáveis
+TB3
+View(TB3)
+
+TB4 <- TB3 %>% 
+  select(-new, -iso2, -iso3)
+View(TB4)
+
+TB5 <- TB4 %>% 
+  separate(sexage, c("sexo", "idade"), sep = 1) 
+TB5
+View(TB5)
+
+#Strings
+require(tidyverse)
+require(magrittr)
+
+#símbolos e seus códigos
+var_com_aspas <-"Ela disse: 'Eu adoro lasanha.'"
+var_com_aspas
+str_view(var_com_aspas)
+
+var_com_aspas3 <- "Ela disse: \"Eu adoro lasanha.\""
+var_com_aspas3
+str_view(var_com_aspas3)
+
+var_com_aspas4 <- 'Ela disse: \'Eu adoro lasanha.\''
+var_com_aspas4
+str_view(var_com_aspas4)
+
+texto_multilinhas <- "Primeira linha\nSegunda linha\nTerceira linha"
+str_view(texto_multilinhas)
+
+texto_tabulado <- "Primeira coluna\tSegunda coluna\tTerceira coluna" 
+str_view(texto_tabulado)
+
+texto_unicode_grau <- "A temperatura é de 25\u00B0C."
+str_view(texto_unicode_grau)
+
+simbolo_somatorio <- "O símolo do somatório é: \u2211"
+str_view(simbolo_somatorio)
+
+emoji <- "OMG! Também posso usar emoji! \U1F631"
+str_view(emoji)
+
+##Concatenando Strings
+df <- data.frame(nome = c("Ana", "Maria", "João", NA), 
+                 sobrenome= c("Santos", "Silva", "Souza", NA))
+df %>% 
+  mutate(ola = str_c("Boa noite ", nome, " ", sobrenome, "!")) #Escrevendo as frases
+
+df %>% 
+  mutate(mensagem = str_glue("Boa noite {nome} {sobrenome}!")) #alternativa
+
+df$nome %>% 
+  paste(., collapse = ", ") #unindo strings
+
+df$nome %>% 
+  str_flatten(na.rm = TRUE) #unindo strings
+
+df$nome %>% 
+  str_flatten(na.rm = TRUE, collapse = ", ", last = " e ") #unindo e acrescentando 'e' 
+
+#Formatação básica
+texto_exemplo = c("caixa baixa", "CAIXA ALTA", "Texto de sentença", "Texto Em Título")
+str_to_lower(texto_exemplo) #caixa baixa
+str_to_sentence(texto_exemplo) #a primeira palavra começa com letra maiúscula
+str_to_title(texto_exemplo) #Todas as palavras com letra maiúscula
+str_to_upper(texto_exemplo) #caixa alta
+
+#retirando espaços nas extremidades
+texto_com_espaços = "  Olá, esse texto tem    diversos        espaços completamente desnecessários. "
+
+str_trim(texto_com_espaços, side = "left")
+str_trim(texto_com_espaços, side = "right")
+str_trim(texto_com_espaços, side = "both")
+str_squish(texto_com_espaços)
+
+#Comprimentos de Strings
+df %>% 
+  mutate(comprimento_nome = str_length(nome)) #tamanho do nome
+df %>% 
+  mutate(qtd_a = str_count(nome, "a")) #contagem de letras
+
+df %>% 
+  mutate(qtd_vogais = str_count(nome, "[aeiou]")) %>% #especificamente minúsculas
+  mutate(qtd_consoantes = str_count(nome, "[^aeiou]")) #mais contagens (^indica o contrário do que foi indicado)
+
+#Subtituição de strings
+df %>% 
+  mutate(nome = str_replace(nome, "ã", "a"))
+df %>% 
+  mutate(nome = str_remove(nome, "[aeiouã]"))
+df %>% 
+  mutate(nome = str_remove(nome, "[aeiouã]"))
+df %>% 
+  mutate(nome = str_remove_all(nome, "[aeiouã]"))
+
+#RegEx - Expressões Regulares
+texto <- "O gato é um animal adorável."
+padrao <- "gato"
+str_detect(texto, padrao) #Detectou se gato está no texto
+
+texto <- c("O rato correu para o buraco.", 
+           "O gato correu para o buraco.",
+           "O mato.")
+padrao <- "(g|r)ato"
+str_detect(texto, padrao) # a | corresponde ao OU lógico
+
+#Âncoras ancoram padrões em posições específicas em strings
+texto <- c("Banana", "Ana", "Ananas")
+padrao <- "^ana"
+str_detect(texto, padrao)
+
+## Ignorar case
+str_detect(texto, "(?i)ana") #(?i) = ignore se é maiúsculo ou minúsculo
+
+## Ignorar case, terminar com ana
+str_detect(texto, "(?i)ana$")
+## Ignorar case, começar com ana
+str_detect(texto, "(?i)^ana")
+## Ignorar case, exatamente  ana
+str_detect(texto, "(?i)^ana$")
+
+#Quando interessado em subexpressões
+str_extract(texto, "(na)+")
+
+#Quantificando
+texto <- "Os números 123 e 456 são importantes."
+padrao <- "\\d{3}"
+numeros <- str_extract_all(texto, padrao, simplify = TRUE)
+numeros
+
+texto <- "Os números 123, 456 e 78 são importantes."
+padrao <- "\\d{2}"
+numeros <- str_extract_all(texto, padrao, simplify = TRUE)
+numeros
